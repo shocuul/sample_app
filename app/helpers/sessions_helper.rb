af4f8d750284845d924d6d88d1e69a372f7a61a3
Jose David Pacheco Valedo <josepdvahn@gmail.com>
2013-07-29 14:46:24 -0500

    Add user micropost

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,6 +14,12 @@ module SessionsHelper
   def current_user?(user)
     user == current_user
   end
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Iniciar Sesion Primero."
+    end
+  end
 	def signed_in?
     !current_user.nil?
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,23 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130727183429) do
+ActiveRecord::Schema.define(:version => 20130729160303) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
     t.string   "password_digest"
     t.string   "remember_token"
-    t.boolean  "admin"
+    t.boolean  "admin",           :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
