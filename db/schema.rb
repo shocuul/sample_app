7ccc4ac6ef5c1da24c0814b4b9208a4ccb7da13b
NeoPoliticaTV <neopoliticatv@localhost.localdomain>
2013-09-03 17:52:48 -0500

    Algo de Estilo al web

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -42,36 +41,6 @@ ActiveRecord::Schema.define(:version => 20130901214308) do
     t.datetime "image_updated_at"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-    t.string   "password_digest"
-    t.string   "remember_token"
-    t.boolean  "admin"
-  end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
-
-end
-=======
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended to check this file into your version control system.
-
-ActiveRecord::Schema.define(:version => 20130729202703) do
-
   create_table "microposts", :force => true do |t|
     t.string   "content"
     t.integer  "user_id"
@@ -104,4 +73,3 @@ ActiveRecord::Schema.define(:version => 20130729202703) do
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
->>>>>>> 4e2da58fe9d5812738607697bfec2bffa6d3701f
